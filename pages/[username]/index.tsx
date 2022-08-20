d62bdaf5a053939cd7e172878f050498ae92aef3
Paul O'Neill <pauloneill08@outlook.com>
2022-08-20 16:53:31 +0100

    feat: SSR main page implemented

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,11 +1,35 @@
 import PostFeed from '../../components/PostFeed';
 import UserProfile from '../../components/UserProfile';
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
 
-export default function UserProfilePage({ user, posts }) {
+export async function getServerSideProps({ query }) {
+    const { username } = query;
+
+    const userDoc = await getUserWithUsername(username);
+    
+    let user = null;
+    let posts = null;
+
+    if (userDoc) {
+        user = userDoc.data();
+        const postsQuery = userDoc.ref
+          .collection('posts')
+          .where('published', '==', true)
+          .orderBy('createdAt', 'desc')
+          .limit(5);
+        posts = (await postsQuery.get()).docs.map(postToJSON);
+      }
+
+    return {
+        props: { user, posts },
+    }
+}
+
+export default function UserProfilePage({ user, posts, admin }) {
     return (
         <main>
             <UserProfile user={user}/>
-            <PostFeed posts={posts} />
+            <PostFeed posts={posts} admin={admin} />
         </main>
     )
 }
